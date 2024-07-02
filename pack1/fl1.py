3ab5f390a6629e12a4ffb8c17aa8ca2a4739ce22
Yousef556677 <yousefawdh0@gmail.com>
2024-07-02 14:06:40 +0300

    try to merge

--- pack1/fl1.py
+++ pack1/fl1.py
@@ -1,3 +1,3 @@
 def fun1():
-    x=int(input("enter your number and i'm going to guess it:"))
+    x=int(input("enter your number and ddi'm going to guess it:"))
     print( f"your Number issss {x}")
